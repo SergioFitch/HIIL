8ba8cf1d235625cdd4a6f11f9e4c172c4d10428c
SergioFitch <so.fitch@gmail.com>
2024-02-29 23:19:28 -0800

    added rest ratio and focus time + about section

--- background.js
+++ background.js
@@ -26,7 +26,7 @@ async function createOffscreen(){
 
 
 
-const randFunction = (input) => {
+const randFunction = (input, restInt) => {
     let min = input*0.8
     let max = input*1.2
     
@@ -34,7 +34,7 @@ const randFunction = (input) => {
  
     const startInterval = Math.max(randNumber, Math.round(min))
 
-    const endInterval = startInterval + 10
+    const endInterval = startInterval + restInt
 
     return {
         restStart: startInterval,
@@ -50,30 +50,26 @@ let breakCounter;
 //setting up an alarm with the timeInput
 chrome.alarms.onAlarm.addListener((alarm)=>{
     if(alarm.name==="HIIL"){
-        chrome.storage.local.get(["timer", "isRunning", "timeInput", "restTime"/*,"numBreaks"*/],
+        chrome.storage.local.get(["timer", "isRunning", "timeInput", "focusTime","restRatio", "restTime"],
             (res)=>{
                 
             if (res.isRunning){
                 let timer = res.timer+1
                 let isRunning = true
-                //numBreaks=(res.timeInput*60)/120 //average time duration of a work bout 
-                //send a message with the sound to be initialized in the offscreen script
-                                
+                let restInt = res.focusTime*60*res.restRatio
                 //create random interval for resting if it doesnt exist yet
                 if(!rand){
-                    rand = randFunction(120); 
+                    rand = randFunction((res.focusTime*60), restInt); 
                     chrome.storage.local.set({restTime:rand}); //here we store that variable in the local storage
                 }
                 //assign restTime to whatever rest interval exists already
                 let restTime = res.restTime || rand;
                 
-                //console.log(`numBreaks: ${numBreaks}`)
-                //console.log(`timer ${timer}`)
-                //console.log(``)
-                //console.log(`rand: {start: ${rand.restStart} , stop: ${rand.restStop} }`)
-               //console.log(`restTime: {start: ${restTime.restStart} , stop: ${restTime.restStop} }`)
-            
-
+                console.log(`timer: ${timer}`)
+                console.log(`restRatio: ${res.restRatio}`)
+                console.log(`focusTime: ${res.focusTime}`)
+                console.log(`restInt: ${restInt}`)
+                console.log(`restTime: {restTime.restStart: ${restTime.restStart}, restTime.restStop: ${restTime.restStop}}`)
                 //if the timer is equal to start of alarm, then set off alarm
                 if (timer === restTime.restStart) {
                     this.registration.showNotification("HIIL", {
@@ -89,7 +85,7 @@ chrome.alarms.onAlarm.addListener((alarm)=>{
                     });
                     createOffscreen();
 
-                    rand = randFunction(120);
+                    rand = randFunction((res.focusTime*60), restInt);
                     rand.restStart += timer
                     rand.restStop += timer
                   
@@ -103,7 +99,12 @@ chrome.alarms.onAlarm.addListener((alarm)=>{
                     })
                     timer = 0
                     isRunning = false
-                    chrome.storage.local.set({restTime:{restStart:0,restStop:0}});
+                    chrome.storage.local.set({
+                        restTime:{restStart:0,restStop:0},
+                        focusTime:2,
+                        restRatio:0.1
+                    
+                    });
                     createOffscreen();
                 }
 
@@ -120,18 +121,20 @@ chrome.alarms.onAlarm.addListener((alarm)=>{
 
 
 //getting and setting the parameters for the timer local storage variables
-chrome.storage.local.get(["timer","isRunning","timeInput","restTime"],(res)=>{
+chrome.storage.local.get(["timer","isRunning","timeInput","restTime", "restRatio","focusTime"],(res)=>{
     chrome.storage.local.set({
         timer:"timer" in res ? res.timer: 0,
         timeInput:"timeInput" in res? res.timeInput:25,
         isRunning: "isRunning" in res?  res.isRunning : false,
-        restTime: null
-        //numBreaks: "numBreaks" in res ? res.numBreaks : 10
+        restTime: null,
+        focusTime: "focusTime" in res? res.focusTime: 2,
+        restRatio: "restRatio" in res? res.restRatio: 0.1
+        
+
     })
 })
 
 
-/*todo: style -> some black and gradient
-                add input for rest length
-                add input for work length
+/*todo: style -> add collapsible for info
+                 add buy me coffee            
 */
--- popup.js
+++ popup.js
@@ -1,21 +1,16 @@
 //https://developer.chrome.com/docs/extensions/get-started
 
 const startTimerBtn = document.getElementById("start-btn")
+const readMeBtn = document.getElementById("about-btn")
+const readMeLbl = document.getElementById("readMe-lbl")
+
 const timeInput = document.getElementById("time-input")
+const focusInput = document.getElementById("focus-input")
+const restInput = document.getElementById("rest-input")
 
-//const timeError = document.getElementById("time-error")
-//const breakError = document.getElementById("break-error")
-
-
-function disableInput(state){
-    let inputElmnt = document.getElementById("time-input");
-    
-        if (state){
-            inputElmnt.disabled = false
-        }else{
-            inputElmnt.disabled = true
-        }   
-}
+const totalLabel = document.getElementById("total-label")
+const focusLabel = document.getElementById("focus-label")
+const restLabel = document.getElementById("rest-label")
 
 /*starting the timer by changing the isRunning variable from false to true*/
 startTimerBtn.addEventListener("click",()=>{
@@ -25,24 +20,47 @@ startTimerBtn.addEventListener("click",()=>{
         timer: !res.isRunning? 0: res.timer,
         isRunning:!res.isRunning  
     },()=>{
-        startTimerBtn.textContent = !res.isRunning? "Reset": "Start",
-        disableInput(res.isRunning);
+        startTimerBtn.textContent = !res.isRunning? "Reset": "Start"
+        disableInput(!res.isRunning);
     }
-       
     )  
 
 })
 })
 
+readMeBtn.addEventListener("click", ()=>{
+    
+    if (readMeLbl.style.display ==="none"){
+        readMeLbl.style.display ="grid";
+    }else{
+        readMeLbl.style.display ="none";
+    }
+})
+
+
+//preventing stupid errors
+function disableInput(state){
+        if (state){
+            timeInput.disabled = true
+            focusInput.disabled = true
+            restInput.disabled = true
+        }else{
+            timeInput.disabled = false
+            focusInput.disabled = false
+            restInput.disabled = false
+        }   
+}
+
 updateTime()
 setInterval(updateTime,1000)
 
-//validating input
+//validating input and passing to storage.local
 
 timeInput.addEventListener("change", (event)=>{
     const val = event.target.value
-    if (val <0 ||  val>90){
+    if (val <5 ||  val>90){
         timeInput.value = 53
+        
       //  timeError.textContent = "Try focusing < 90 min at a time"
     }else{
         //timeError.textContent =""
@@ -56,28 +74,42 @@ timeInput.addEventListener("change", (event)=>{
     }
 })
 
-/* 
-numBreaks.addEventListener("change", (event)=>{
-    let breakVal = event.target.value
-    let workInterval = (timeInput.value*60)/breakVal //360 seconds in last run
-    let restInterval = timeInput.value*60*0.1 // the restInterval should be equal to this    ->
-    
-    if( workInterval < restInterval ){ //if it's higher than a number that leaves less than an equal amount of rest between the rest and work intervals
-        breakError.textContent = `Try resting less ${breakVal} : ${timeInput.value}` 
-
+focusInput.addEventListener("change", (event)=>{
+    const val = event.target.value
+    let halfTime = timeInput.value/2
+    if (val<1 || val>halfTime){
+            focusLabel.textContent = `focus 1<>${halfTime}min`
+            startTimerBtn.disabled=true
     }else{
-        breakError.textContent =""
-        numBreaks.value =event.target.value
+        focusLabel.textContent= "Focus Time (min):"
+        focusInput.value =event.target.value
         chrome.storage.local.set({
-            numBreaks: event.target.value
+            focusTime: event.target.value    
+        })
+    }
+
+})
+
+restInput.addEventListener("change", (event)=>{
+    const val =event.target.value
+
+    if( val == "F:R"){
+        startTimerBtn.disabled=true
+    }else{
+        chrome.storage.local.set({
+            restRatio: event.target.value
         })
     }
 })
-*/
+
 // 
-chrome.storage.local.get(["timeInput", "isRunning"], (res)=>{
-    timeInput.value = res.timeInput,
+chrome.storage.local.get(["timeInput", "isRunning", "focusTime", "restRatio","isRunning"], (res)=>{
+    timeInput.value = res.timeInput
+    focusInput.value = res.focusTime
+    restInput.value = res.restRatio
     startTimerBtn.textContent = !res.isRunning? "Start": "Reset"
+    
+    disableInput(res.isRunning);
 }
 )
 
